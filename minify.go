3f9fae792d30dd72e9b6b00779d6d4ad37b6847c
jmarcantony <josephmarc2610@gmail.com>
2022-01-03 19:54:28 +0400

    refactor: remove unnecasary code

--- minify.go
+++ minify.go
@@ -9,28 +9,22 @@ import (
 )
 
 func main() {
-    args := os.Args[1:]
-    if len(args) == 0 {
-        log.Fatalln("Pass a file path to minify")
-    }
-    filename := args[0]
-    if !strings.HasSuffix(filename, ".go") {
-        log.Fatalln("Not a go file!")
-    }
-    file, err := ioutil.ReadFile(filename)
-    if err != nil {
-        log.Fatal(err)
-    }
-    file, c := RemoveNewLine(file)
-    file = RemoveSpaces(file)
-    file = RemoveSemiColon(file, c)
-    var f []byte
-    for _, v := range file {
-        if v != 0 {
-            f = append(f, v)
-        }
-    }
-    for _, b := range f {
-        fmt.Printf("%c", b)
-    }
+	args := os.Args[1:]
+	if len(args) == 0 {
+		log.Fatalln("Pass a file path to minify")
+	}
+	filename := args[0]
+	if !strings.HasSuffix(filename, ".go") {
+		log.Fatalln("Not a go file!")
+	}
+	file, err := ioutil.ReadFile(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	file, c := RemoveNewLine(file)
+	file = RemoveSpaces(file)
+	file = RemoveSemiColon(file, c)
+	for _, b := range file {
+		fmt.Printf("%c", b)
+	}
 }
--- remove_semicolon.go
+++ remove_semicolon.go
@@ -1,31 +1,31 @@
 package main
 
-/*
-package main;;import(;"fmt";"math";);;;func squareNum(i int){;fmt.Println(math.Pow(float64(i),2));};func main(){;for i:=0;i<11;i++{;squareNum(i);};};
-*/
-
 func checkBetween(b byte, i int, f []byte) bool {
-    if (i-1 < 0) || (i + 1 >= len(f)) {
-        return false
-    }
-    if !IsAlphabet(f[i-1]) && !IsAlphabet(f[i+1]) {
-        return true
-    }
-    return false
+	if (i-1 < 0) || (i+1 >= len(f)) {
+		return false
+	}
+	if f[i-1] == '"' || f[i+1] == '"' {
+		return false
+	}
+	if !IsAlphabet(f[i-1]) && !IsAlphabet(f[i+1]) {
+		return true
+	}
+	return false
 }
 
 func RemoveSemiColon(f []byte, c map[int]bool) []byte {
-    var prev byte
-    for i, v := range f {
-        if _, ok := c[i]; ok {
-            continue
-        }
-        if v == ';' {
-            if prev == ';' || checkBetween(v, i, f) {
-                f[i] = 0
-            }
-        }
-        prev = f[i]
-    }
-    return f
+	var prev byte
+	for i, v := range f {
+		if _, ok := c[i]; ok {
+			continue
+		}
+		if v == ';' {
+			if prev == ';' || checkBetween(v, i, f) {
+				f[i] = 0
+			}
+		}
+		prev = f[i]
+	}
+	f[len(f)-1] = 0
+	return f
 }
